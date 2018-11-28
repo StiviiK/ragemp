87c7abee9d666da05138f8d23c3452793067f72f
Stefan Kürzeder <stefan.kuerzeder@gmail.com>
2018-11-28 13:12:25 +0000

    Moved entrypoint into /debian and /alpine

---
+++ debian/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+if [ ! -e bridge/settings.xml ]; then
+	echo "Downloading bridge default files..."
+
+	mkdir tmp
+	mkdir bridge
+	curl http://cdn.gtanet.work/bridge-package-linux.tar.gz -o tmp/bridge.tar.gz
+	tar -xzf tmp/bridge.tar.gz -C tmp/
+	mv tmp/bridge/* bridge/
+	mv tmp/conf.json .
+	rm -rf tmp
+
+	echo "Done."
+fi
+
+exec ./server
